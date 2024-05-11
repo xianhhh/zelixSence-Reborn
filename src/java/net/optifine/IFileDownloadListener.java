acd00138113375401055e0de809597e321bfcd2a
xianhhh <zeh5741@163.com>
2024-05-11 16:34:37 +0800

    cvs

--- src/java/net/optifine/IFileDownloadListener.java
+++
@@ -1,6 +0,0 @@
-package net.optifine;
-
-public interface IFileDownloadListener
-{
-    void fileDownloadFinished(String var1, byte[] var2, Throwable var3);
-}
--- src/java/net/optifine/IOptionControl.java
+++
@@ -1,8 +0,0 @@
-package net.optifine;
-
-import net.minecraft.client.settings.GameSettings;
-
-public interface IOptionControl
-{
-    GameSettings.Options getOption();
-}
